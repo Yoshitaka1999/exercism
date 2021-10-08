25a82842a5661e0d06483e2674aa964bc6622d7b
加藤義隆 <yoshitakakato@katouyoshitakanoMacBook-Air.local>
2021-10-08 10:44:21 +0900

    matrix and series

---
+++ ruby/series/series.rb
@@ -0,0 +1,10 @@
+class Series
+    def initialize(series)
+        @series = series
+    end
+
+    def slices(num)
+        raise ArgumentError if num > @series.length 
+        @series.chars.each_cons(num).map(&:join)
+    end
+end
